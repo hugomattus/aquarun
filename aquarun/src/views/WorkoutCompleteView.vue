95dff2de81a3ce09477f43bb849b47e8be4e15b4
Hugo Matos <Hugomattusdd@gmail.com>
2026-07-24 16:20:38 -0300

    fix: remove white number input spinners

--- aquarun/src/views/WorkoutCompleteView.vue
+++ aquarun/src/views/WorkoutCompleteView.vue
@@ -540,3 +540,14 @@ onMounted(async () => {
   }
 })
 </script>
+
+<style scoped>
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
